9d42dd94e6ef87e7c7bda993c8be4634c4aef601
BinBuddyUK <georgekaianderson@gmail.com>
2025-07-13 00:27:22 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] Install');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
